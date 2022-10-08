8bf764293c106c0d781365f1502ab9f3bb428433
imswaggyboy <75663923+imswaggyboy@users.noreply.github.com>
2022-10-08 19:41:19 +0530

    Update datalemur.sql

--- datalemur.sql
+++ datalemur.sql
@@ -1,5 +1,14 @@
+----------------EASY QUESTIONS-------------------
 --Q1. data Science Skills
 SELECT DISTINCT(candidate_id) FROM candidates
 WHERE skill in ('Python','Tableau','PostgreSQL')
 GROUP BY candidate_id
 having COUNT(candidate_id) = 3;
+
+-- Q2. Page With No Likes
+SELECT p.page_id  
+FROM pages p
+LEFT JOIN page_likes pl
+on p.page_id = pl.page_id
+where pl.liked_date is NULL
+order by page_id ASC;
